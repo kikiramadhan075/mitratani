8e81bcd1e12c59a83770f5edf058039341da2b7e
kikiramadhan075 <kikiramadhan028@gmail.com>
2024-01-05 00:00:19 +0700

    add login page and register page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.dashboard');
 });
+
+Route::get('/login', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('register', function () {
+    return view('pages.auth.register');
+})->name('register');
